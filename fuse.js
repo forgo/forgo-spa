663622cc235c89679fac03de0609bf4174864187
forgo <elliott.richerson@gmail.com>
2019-03-04 14:30:34 -0700

    random experiments

--- fuse.js
+++ fuse.js
@@ -24,25 +24,15 @@ context(
         sourceMaps: !this.isProduction,
         allowSyntheticDefaultImports: true,
         target: 'browser@es5',
+        useTypescriptCompiler : true,
         definedExpressions: {
           "__isBrowser__": true,
         },
         plugins: [
           CSSPlugin(),
-          // WebIndexPlugin({
-          //   // template: `${SRC_PATH}templates/index.html`,
-          //   // title: 'Is this working?',
-          //   // appendBundles: true,
-          //   // bundles: ['api',BUNDLE],
-          //   path: './'
-          // }),
-          // QuantumPlugin({
-          //   treeshake: true,
-          //   uglify: this.isProduction,
-          //   definedExpressions: {
-          //     "__isBrowser__": true,
-          //   },
-          // }),
+          WebIndexPlugin({
+            template: `${SRC_PATH}templates/index.html`,
+          }),
         ],
       }
     }
@@ -51,7 +41,7 @@ context(
 
 // clean task
 task('clean', context =>  {
-  src(BUILD_PATH).clean(BUILD_PATH).exec()
+  return src(BUILD_PATH).clean(BUILD_PATH).exec()
 })
 
 // config task
--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,47 @@ import React from 'react'
 import { render } from 'react-dom'
 import MonacoEditor from 'react-monaco-editor'
 
+//https://github.com/Microsoft/monaco-editor/blob/master/docs/integrate-esm.md
+import { editor } from 'monaco-editor/dev/vs/editor/editor.main.js';
+
+console.log("editor:", editor)
+
+// Since packaging is done by you, you need
+// to instruct the editor how you named the
+// bundles that contain the web workers.
+self.MonacoEnvironment = {
+  getWorkerUrl: function (moduleId, label) {
+    if (label === 'json') {
+      return require('monaco-editor/esm/vs/language/json/json.worker');
+    }
+    if (label === 'css') {
+      return require('monaco-editor/esm/vs/language/css/css.worker');
+    }
+    if (label === 'html') {
+      return require('monaco-editor/esm/vs/language/html/html.worker');
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return require('monaco-editor/esm/vs/language/typescript/ts.worker');
+    }
+    return require('monaco-editor/esm/vs/editor/editor.worker.js');
+  }
+}
+
+editor.create(document.getElementById('container'), {
+  value: [
+    'function x() {',
+    '\tconsole.log("Hello world!");',
+    '}'
+  ].join('\n'),
+  language: 'javascript'
+});
+
+
 export default class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      code: '// type your code...',
+      code: '// type your code..\nconst x = 23;',
     }
   }
   editorDidMount(editor, monaco) {
@@ -14,18 +50,22 @@ export default class App extends React.Component {
     editor.focus()
   }
   onChange(newValue, e) {
+
     console.log('onChange', newValue, e)
   }
   render() {
     const code = this.state.code
     const options = {
-      selectOnLineNumbers: true
+      selectOnLineNumbers: true,
+      colorDecorators: true,
+      rulers: [1,13,27],
+      formatOnType: true,
     };
     return (
       <MonacoEditor
         width="800"
         height="600"
-        language="javascript"
+        language="css"
         theme="vs-dark"
         value={code}
         options={options}
